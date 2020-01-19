1d56d09bd5b55861c03bf9dae563a50e1a4eb073
yusts <stasivyura@gmail.com>
2020-01-19 20:16:28 +0100

    face-detection

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controlers/register');
+const signin = require('./controlers/signin');
+const profile = require('./controlers/profile');
+const image = require('./controlers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      connectionString : process.env.DATABASE_URL,
+      ssl: true
+    }
+}); 
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res)=> {
+    res.send('it\'s workong');
+})
+app.post('/signin', (req, res) => {
+    signin.handleSignin(req, res, db, bcrypt)
+})
+app.post('/register', (req, res) => {
+    register.handleRegister(req, res, db, bcrypt)
+})
+app.get('/profile/:id', (req, res) => {
+    profile.handleProfile(req, res, db)
+})
+app.put('/image', (req, res) => {
+    image.handleImage(req, res, db)
+})
+app.post('/imageurl', (req, res) => {
+    image.handleApiCall(req, res)
+})
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+});
+
